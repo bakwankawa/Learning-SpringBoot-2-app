1571bc4ebdabaf471777e6422892cf780bdaf758
mahatma kawa <mahatmakawa@gmail.com>
2022-10-14 15:32:24 +0700

    angular components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employeemanagerapp';
+export class AppComponent implements OnInit {// whenever it initialize will call getEmployees function
+  public employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService) {}  // inject the service to this app component
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {   // this method return void
+    this.employeeService.getEmployees().subscribe(    // this is to call service. Because this method is unobservable,
+                                                      // then we call getEmployees from service that observable and
+                                                      // using subscribe to notify to data what we get
+      (response: Employee[]) => {  // if the response is Employees[]
+        this.employees = response;  // it will give whatever response takes
+      },
+      (error: HttpErrorResponse) => {   // elif the response is error
+        alert(error.message);
+      }
+    );
+  }
 }
--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,30 +1,30 @@
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { Employee } from "./employee";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'  // this is to let entire angular app know about this service, other method is define in app modules.
 })
-
 export class EmployeeService {
-    private apiServerUrl = '';
+  private apiServerUrl = environment.apiBaseUrl;
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}  // inject http client to make communication with server
 
-    public getEmployees(): Observable<Employee[]> {     // <Employee[]> pass the class from Employee to be returned from this method
-        return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
-    }
+  public getEmployees():Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+  }
 
-    public addEmployee(employee: Employee): Observable<Employee> {      // take the actual employee we're adding and return one employee
-                                                                        // because no bracket []
-        return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);     // pass the payload employee as second argument
-    }
+  public addEmployees(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+  }
 
-    public updateEmployee(employee: Employee): Observable<Employee> {
-        return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
-    }
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+  }
 
-    public deleteEmployee(employeeId: number): Observable<void> {
-        return this.http.post<Employee>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
-    }
+  public deleteEmployee(employeeId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+  }
 }
