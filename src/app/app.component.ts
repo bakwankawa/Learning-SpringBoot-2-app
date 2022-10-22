9eab01b930be682d011ddca5de35ae78939948df
mahatma kawa <mahatmakawa@gmail.com>
2022-10-22 13:23:03 +0700

    ui modal logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,4 +29,29 @@ export class AppComponent implements OnInit {// whenever it initialize will call
       }
     );
   }
+
+  /*
+  This function is according to https://getbootstrap.com/docs/5.1/components/modal/.
+  And this button is only available in typescript only, to make this button appear
+  in html we need to access the container from html main div and call deriven 'click'
+  object that we derivate from this function.
+   */
+  public onOpenModal(mode: string, employee?: Employee): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if (mode === 'edit') {
+      button.setAttribute('data-target', '#editEmployeeModal');
+    }
+    if (mode === 'delete') {
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    container?.appendChild(button);
+    button.click();
+  }
 }
